067244b53d784b0d555b8b893de389f1106c2952
Cella Stowitts <78512719+cstowitts@users.noreply.github.com>
2022-04-11 12:32:30 -0700

    Adds methods to LinkedList class -push -unshift -pop -shift -getAt

--- linked-list.js
+++ linked-list.js
@@ -24,11 +24,36 @@ class LinkedList {
 
   push(val) {
 
+    const newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+      this.length++;
+    }
+
+    const secondToLast = this.tail;
+    secondToLast.next = newNode;
+    this.tail = newNode;
+    this.length++;
+
   }
 
   /** unshift(val): add new value to start of list. */
 
   unshift(val) {
+
+    const newNode = new Node(val);
+
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+      this.length++;
+    }
+
+    const newSecond = this.head;
+    this.head = newNode;
+    this.head.next = newSecond;
+    this.length++;
 
   }
 
@@ -36,17 +61,73 @@ class LinkedList {
 
   pop() {
 
+    if (this.length === 0) {
+      throw new Error("This LinkedList is empty!");
+    }
+
+    if (this.length === 1) {
+      const oldTail = this.tail;
+      this.head = null;
+      this.tail = null;
+      this.length--;
+      return oldTail;
+
+    } else {
+      const oldTail = this.tail;
+      let current = this.head;
+
+      while (current !== oldTail) {
+
+        if (current.next === oldTail) {
+          this.tail = current;
+          current.next = null;
+          return oldTail;
+        }
+        current = current.next
+      }
+    }
+
   }
 
   /** shift(): return & remove first item. */
 
   shift() {
 
+    if(this.length === 0){
+      throw new Error("This LinkedList is empty!");
+    }
+
+    if(this.length === 1){
+      const oldHead = this.head;
+      this.head = null;
+      this.tail = null;
+      return oldHead;
+    }
+
+    const oldHead = this.head;
+    this.head = oldHead.next;
+    return oldHead;
+
   }
 
   /** getAt(idx): get val at idx. */
 
   getAt(idx) {
+
+    if(idx < 0 || idx > this.length-1){
+      throw new Error("That's not a valid index, my guy!");
+    }
+
+    let currIdx = 0;
+    let current = this.head;
+
+    while(currIdx < this.length){
+      if(currIdx === idx){
+        return current.val;
+      }
+      currIdx++;
+      current = current.next
+    }
 
   }
 
@@ -71,8 +152,17 @@ class LinkedList {
   /** average(): return an average of all values in the list */
 
   average() {
-    
+
   }
 }
+
+/**
+ * -if linkedlist has nothing in it (+ if you wanna add to empty LL)
+ * -if you removed from LL with one node
+ * -removing 2nd to last node
+ * -removing 1st node
+ * -removing last node
+ */
+
 
 module.exports = LinkedList;
