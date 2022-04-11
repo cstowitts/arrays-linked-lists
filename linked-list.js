b7c5c866b57a24823e9f4baf3bbc996ed90de2bc
Cella Stowitts <78512719+cstowitts@users.noreply.github.com>
2022-04-11 16:33:18 -0700

    WIP adds methods to LinkedList class -insertAt -average -WIP removeAt, this.tails doesn't update properly -all but one test passes

--- linked-list.js
+++ linked-list.js
@@ -1,193 +1,245 @@
 /** Node: node for a singly linked list. */
 
 class Node {
-	val = null;
-	next = null;
+  val = null;
+  next = null;
 
-	constructor(val) {
-		this.val = val;
-	}
+  constructor(val) {
+    this.val = val;
+  }
 }
 
 /** LinkedList: chained together nodes. */
 
 class LinkedList {
-	head = null;
-	tail = null;
-	length = 0;
+  head = null;
+  tail = null;
+  length = 0;
 
-	constructor(vals = []) {
-		for (let val of vals) this.push(val);
-	}
+  constructor(vals = []) {
+    for (let val of vals) this.push(val);
+  }
 
-	/** push(val): add new value to end of list. */
+  /** push(val): add new value to end of list. */
 
-	push(val) {
-		const newNode = new Node(val);
-		if (this.length === 0) {
-			this.head = newNode;
-			this.tail = newNode;
-			this.length++;
-		} else {
-			const secondToLast = this.tail;
-			secondToLast.next = newNode;
-			this.tail = newNode;
-			this.length++;
-		}
-	}
+  push(val) {
+    const newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+      this.length++;
+    } else {
+      const secondToLast = this.tail;
+      secondToLast.next = newNode;
+      this.tail = newNode;
+      this.length++;
+    }
+  }
 
-	/** unshift(val): add new value to start of list. */
+  /** unshift(val): add new value to start of list. */
 
-	unshift(val) {
-		const newNode = new Node(val);
+  unshift(val) {
+    const newNode = new Node(val);
 
-		if (this.length === 0) {
-			this.head = newNode;
-			this.tail = newNode;
-			this.length++;
-		} else {
-			const newSecond = this.head;
-			this.head = newNode;
-			this.head.next = newSecond;
-			this.length++;
-		}
-	}
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+      this.length++;
+    } else {
+      const newSecond = this.head;
+      this.head = newNode;
+      this.head.next = newSecond;
+      this.length++;
+    }
+  }
 
-	/** pop(): return & remove last item. */
+  /** pop(): return & remove last item. */
 
-	pop() {
-		if (this.length === 0) {
-			throw new Error('This LinkedList is empty!');
-		}
+  pop() {
+    if (this.length === 0) {
+      throw new Error('This LinkedList is empty!');
+    }
 
-		if (this.length === 1) {
-			const oldTail = this.tail;
-			this.head = null;
-			this.tail = null;
-			this.length--;
-			return oldTail.val;
-		} else {
-			const oldTail = this.tail;
-			let current = this.head;
+    if (this.length === 1) {
+      const oldTail = this.tail;
+      this.head = null;
+      this.tail = null;
+      this.length--;
+      return oldTail.val;
+    } else {
+      const oldTail = this.tail;
+      let current = this.head;
 
-			while (current !== oldTail) {
-				if (current.next === oldTail) {
-					this.tail = current;
-					current.next = null;
-					this.length--;
-					return oldTail.val;
-				}
-				current = current.next;
-			}
-		}
-	}
+      while (current !== oldTail) {
+        if (current.next === oldTail) {
+          this.tail = current;
+          current.next = null;
+          this.length--;
+          return oldTail.val;
+        }
+        current = current.next;
+      }
+    }
+  }
 
-	/** shift(): return & remove first item. */
+  /** shift(): return & remove first item. */
 
-	shift() {
-		if (this.length === 0) {
-			throw new Error('This LinkedList is empty!');
-		}
+  shift() {
+    if (this.length === 0) {
+      throw new Error('This LinkedList is empty!');
+    }
 
-		if (this.length === 1) {
-			const oldHead = this.head;
-			this.head = null;
-			this.tail = null;
-			this.length--;
-			return oldHead.val;
-		} else {
-			const oldHead = this.head;
-			this.head = oldHead.next;
-			this.length--;
-			return oldHead.val;
-		}
-	}
+    if (this.length === 1) {
+      const oldHead = this.head;
+      this.head = null;
+      this.tail = null;
+      this.length--;
+      return oldHead.val;
+    } else {
+      const oldHead = this.head;
+      this.head = oldHead.next;
+      this.length--;
+      return oldHead.val;
+    }
+  }
 
-	/** getAt(idx): get val at idx. */
+  /** getAt(idx): get val at idx. */
 
-	getAt(idx) {
-		if (idx < 0 || idx > this.length - 1) {
-			throw new Error("That's not a valid index, my guy!");
-		}
+  getAt(idx) {
+    if (idx < 0 || idx > this.length - 1) {
+      throw new Error("That's not a valid index, my guy!");
+    }
 
-		let currIdx = 0;
-		let current = this.head;
+    let currIdx = 0;
+    let current = this.head;
 
-		while (currIdx < this.length) {
-			if (currIdx === idx) {
-				return current.val;
-			}
-			currIdx++;
-			current = current.next;
-		}
-	}
+    while (currIdx < this.length) {
+      if (currIdx === idx) {
+        return current.val;
+      }
+      currIdx++;
+      current = current.next;
+    }
+  }
 
-	/** setAt(idx, val): set val at idx to val */
+  /** setAt(idx, val): set val at idx to val */
 
-	setAt(idx, val) {
-		if (idx < 0 || idx > this.length - 1) {
-			throw new Error("That's not a valid index, my guy!");
-		}
+  setAt(idx, val) {
+    if (idx < 0 || idx > this.length - 1) {
+      throw new Error("That's not a valid index, my guy!");
+    }
 
-		let currIdx = 0;
-		let current = this.head;
+    let currIdx = 0;
+    let current = this.head;
 
-		while (currIdx < this.length) {
-			if (currIdx === idx) {
-				current.val = val;
-			}
-			currIdx++;
-			current = current.next;
-		}
-	}
+    while (currIdx < this.length) {
+      if (currIdx === idx) {
+        current.val = val;
+      }
+      currIdx++;
+      current = current.next;
+    }
+  }
 
-	/** insertAt(idx, val): add node w/val before idx. */
+  /** insertAt(idx, val): add node w/val before idx. */
 
-	insertAt(idx, val) {
-		if (idx < 0 || idx > this.length + 1) {
-			throw new Error("That's not a valid index, my guy!");
-		}
+  insertAt(idx, val) {
+    if (idx < 0 || idx > this.length + 1) {
+      throw new Error("That's not a valid index, my guy!");
+    }
 
-		let newNode = new Node(val);
+    let newNode = new Node(val);
 
-		if (this.length === 0) {
-			this.head = newNode;
-			this.tail = newNode;
-			this.length++;
-			return;
-		}
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+      this.length++;
+      return;
+    }
 
-		let currIdx = 0;
-		let current = this.head;
+    let currIdx = 0;
+    let current = this.head;
 
-		while (currIdx < this.length) {
-			console.log('tail:', this.tail, 'length:', this.length, 'currIdx:', currIdx);
+    while (currIdx < this.length) {
+      console.log('tail:', this.tail, 'length:', this.length, 'currIdx:', currIdx);
 
-			if (currIdx === idx - 1) {
-				let nodeBefore = current;
-				newNode.next = nodeBefore.next;
-				nodeBefore.next = newNode;
+      if (currIdx === idx - 1) {
+        let nodeBefore = current;
+        newNode.next = nodeBefore.next;
+        nodeBefore.next = newNode;
 
-				if (idx === this.length + 1) {
-					this.tail = newNode;
-				}
+        if (idx === this.length + 1) {
+          this.tail = newNode;
+        }
 
-				this.length++;
-				return;
-			}
+        this.length++;
+        return;
+      }
 
-			currIdx++;
-			current = current.next;
-		}
-	}
+      currIdx++;
+      current = current.next;
+    }
+  }
 
-	/** removeAt(idx): return & remove item at idx, */
+  /** removeAt(idx): return & remove item at idx, */
 
-	removeAt(idx) {}
+  removeAt(idx) {
 
-	/** average(): return an average of all values in the list */
+    if (idx < 0 || idx > this.length) {
+      throw new Error("That's not a valid index, my guy!");
+    }
 
-	average() {}
+    if (this.length === 1) {
+      const soloNode = this.head;
+      this.head = null;
+      this.tail = null;
+      this.length--;
+      return soloNode;
+    }
+
+    let currIdx = 0;
+    let current = this.head;
+
+    while (currIdx !== idx) {
+
+      if (currIdx === idx - 1) {
+        const beforeNode = current;
+        const nodeToRemove = current.next;
+        const afterNode = current.next.next;
+        if (afterNode === null) {
+          this.tail = beforeNode;
+        } else {
+          this.tail = afterNode;
+        }
+        beforeNode.next = afterNode;
+        this.length--;
+        return nodeToRemove;
+      }
+      currIdx++;
+      current = current.next;
+    }
+  }
+
+  /** average(): return an average of all values in the list */
+
+  average() {
+
+    if (this.length === 0) {
+      return 0;
+    }
+
+    let current = this.head;
+    let sum = 0;
+
+    for (let i = 0; i < this.length; i++) {
+      sum += current.val;
+      current = current.next;
+    }
+
+    const avg = sum / this.length;
+    return avg;
+
+  }
 }
 
 /**
